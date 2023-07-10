5bfb3bbe315fc74a3607862957cc2d859773017d
23122K <hazy.parader-0r@icloud.com>
2023-07-10 12:10:50 +0200

    Initial commit

---
+++ StreamingApp/StreamingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingAppApp.swift
+//  StreamingApp
+//
+//  Created by Patryk Maciąg on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct StreamingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
